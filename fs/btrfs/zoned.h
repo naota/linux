2c4dc5955cd689c32c2a2460ef788427560df6fe
Naohiro Aota <naohiro.aota@wdc.com>
2020-10-02 03:36:41 +0900

    btrfs: support dev-replace in ZONED mode
    This is 4/4 patch to implement device-replace on ZONED mode.
    
    Even after the copying is done, the write pointers of the source device and
    the destination device may not be synchronized. For example, when the last
    allocated extent is freed before device-replace process, the extent is not
    copied, leaving a hole there.
    
    This patch synchronize the write pointers by writing zeros to the
    destination device.
    
    Signed-off-by: Naohiro Aota <naohiro.aota@wdc.com>

--- fs/btrfs/zoned.h
+++ fs/btrfs/zoned.h
@@ -54,6 +54,8 @@ void btrfs_revert_meta_write_pointer(struct btrfs_block_group *cache,
 				     struct extent_buffer *eb);
 int btrfs_zoned_issue_zeroout(struct btrfs_device *device, u64 physical,
 			      u64 length);
+int btrfs_sync_zone_write_pointer(struct btrfs_device *tgt_dev, u64 logical,
+				  u64 physical_start, u64 physical_pos);
 #else /* CONFIG_BLK_DEV_ZONED */
 static inline int btrfs_get_dev_zone(struct btrfs_device *device, u64 pos,
 				     struct blk_zone *zone)
@@ -143,6 +145,12 @@ btrfs_revert_meta_write_pointer(struct btrfs_block_group *cache,
 }
 static inline int btrfs_zoned_issue_zeroout(struct btrfs_device *device,
 					    u64 physical, u64 length)
+{
+	return -EOPNOTSUPP;
+}
+static inline int btrfs_sync_zone_write_pointer(struct btrfs_device *tgt_dev,
+						u64 logical, u64 physical_start,
+						u64 physical_pos)
 {
 	return -EOPNOTSUPP;
 }
